6ec74dc78473324f26f25912f83c3623bf3a695d
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-09-28 06:27:22 -0700

    initial commit

--- app.js
+++ app.js
@@ -3,8 +3,48 @@
 // }
 // console.log(falsyOrTruthy(10, 5));
 
-function arrLength(arr) {
-    return arr.length;
+// function arrLength(arr) {
+//     return arr.length;
+// }
+
+// console.log(arrLength([4]));
+
+// function arrSum(arr) {
+//     for (let i = 0; i < arr.length; ++i) {
+//         //This is how you loop theough every element in an array
+        
+//         console.log(i);
+//     }
+//     console.log('for loop broke')
+// }
+
+// console.log(arrSum([1, 2, 3, 4, 5, 6]));
+
+// function arrSum(arr) {
+//     let sum = 0;
+//     for (let i =0; i < arr.length - 1; ++i) {
+//         //This is how you loop through every element in an array
+//         sum = sum + arr[i];
+//     }
+//     return sum;
+// }
+
+// console.log(arrSum([2, 2, 2]));
+
+
+// function progressiveSum(num) {
+//     let sum = 0; 
+//     for(let i = 1; i <= num; ++i) {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+
+// console.log(progressiveSum(3));
+
+function calcTime(seconds) {
+    return seconds / 60;
+    let timerSeconds = seconds % 60;
 }
 
-console.log(arrLength([4]));
+console.log(calcTime(180));
