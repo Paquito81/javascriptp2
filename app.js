67056fe70b6adda7b24642e18ee4e04bf026d560
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-09-28 04:41:39 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// function falsyOrTruthy(elem1, elem2) {
+//     return (!elem1) ? elem1 : elem2;
+// }
+// console.log(falsyOrTruthy(10, 5));
+
+function arrLength(arr) {
+    return arr.length;
+}
+
+console.log(arrLength([4]));
