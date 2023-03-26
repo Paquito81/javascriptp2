ad4f46a8af399366a99a0826542b652b42ebfcc9
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2023-03-26 06:08:14 -0700

    Javascipt

--- app.js
+++ app.js
@@ -1,13 +1,68 @@
+/** 
+ * Question 1. Falsy or Truthy?
+ * Given two values, return the first one if it is falsy, otherwise return the second one.
+ * filterOutFalsy(0, 500) -> 0
+ * filterOutFalsy(false, 100) -> False
+ * filterOutFalsy([true, 'Dog']) -> 'Dog'
+*/
+
+// function falsyOrTruthy(elem1, elem2) {
+//     if (/* falsy */) {
+//         // return the element
+//     }
+//     else {
+//         // return element 2
+//     }
+// }
+
 // function falsyOrTruthy(elem1, elem2) {
 //     return (!elem1) ? elem1 : elem2;
 // }
 // console.log(falsyOrTruthy(10, 5));
 
+/** 
+ * Question 2. Return the length of any given array
+ * Given an array, return its length.
+ * 
+ * arrLength([1, 2, 3]) -> 3
+ * arrLength([5, 0, -4, 1]) -> 4
+ * arrLength([]) -> 0 
+*/
+
 // function arrLength(arr) {
 //     return arr.length;
 // }
 
-// console.log(arrLength([4]));
+// console.log(arrLength([1, 2, 3,]));
+// console.log(arrLength([5, 0, -4, 1]));
+// console.log(arrLength([]));
+
+/**
+ * Question 3. Get the last element in an array
+ * Given an array, return the first element.
+ * 
+ * lastElem([3, 2, 0, 6, 2]) -> 2
+ * lastElem(['dog', 'cat', 'ball']) -> 'ball'
+ * lastElem([null, 5, false]) -> false
+ */
+
+// function arrLength(arr) {
+//     return arr[arr.length -1];
+// }
+// console.log(arrLength([3, 2, 0, 6, 2]))
+// console.log(arrLength(['dog', 'cat', 'ball']));
+// console.log(arrLength([null, 5, false]));
+
+// console.log(arrLength([0, 1, 2, 3]));
+
+/**
+ * Question 4. Find the sum of an array
+ * Given the array, return the sum of every element.
+ * 
+ * arrSum([2, 2, 2]) -> 6
+ * arrSum([100, 200, 500]) -> 800
+ * arrSum([0, -5, -10]) -> -15
+ */
 
 // function arrSum(arr) {
 //     for (let i = 0; i < arr.length; ++i) {
@@ -18,18 +73,32 @@
 //     console.log('for loop broke')
 // }
 
+// console.log(arrSum([100, 200, 500]));
+// console.log(arrSum([2, 2, 2])); 
 // console.log(arrSum([1, 2, 3, 4, 5, 6]));
 
 // function arrSum(arr) {
-//     let sum = 0;
-//     for (let i =0; i < arr.length - 1; ++i) {
-//         //This is how you loop through every element in an array
+//     let sum = 0
+//     for (let i = 0; i < arr.length; ++i) {
+//         //This is how you loop theough every element in an array
 //         sum = sum + arr[i];
 //     }
 //     return sum;
 // }
 
-// console.log(arrSum([2, 2, 2]));
+// console.log(arrSum([100, 200, 500]));
+// console.log(arrSum([2, 2, 2])); 
+// console.log(arrSum([1, 2, 3, 4, 5, 6]));
+
+/**
+ * Question 5. Add up numbers from a single number
+ * Given a number, add up all the numbers from one to the number that is given. 
+ * E.g. An input of 4 will give you 1 + 2 + 3 + 4, which equals 10.
+ * progressiveSum(3) -> 6
+ * progressiveSum(4) -> 10
+ * progressiveSum(600) -> 180300
+ */
+
 
 
 // function progressiveSum(num) {
@@ -41,6 +110,14 @@
 // }
 
 // console.log(progressiveSum(3));
+// console.log(progressiveSum(4));
+// console.log(progressiveSum(600));
+
+/**
+ * Question 6. Calculate the time
+ * Given the number in seconds, return this number in mm:ss format.
+ *  
+ */
 
 // function calcTime(seconds) {
 //     let timerMinutes = Math.floor(seconds / 60);
@@ -53,17 +130,32 @@
 //     return  timerMinutes + ':' + timerSeconds
 // }
 
-// console.log(calcTime(500));
+// console.log(calcTime(66));
+// console.log(calcTime(50));
+// console.log(calcTime(300));
 
-// function getMax(arr) {
-//     let max = arr[0];
-//     for (let i = 0; i < arr.length; ++i) {
-//         if (arr[i] > max) {
-//             max = arr[i]
-//         }
-//     }
-//     return max;
-// }
+
+/**
+ * Question 7. Find the largest number
+ * Given an array of numbers, return the largest number of that array
+ * 
+ * getMax([5, 100, 0]) ->100
+ * getMax([12, 10 ,-20]) -> 12
+ * getMaxl([-300, -100, -200]) -> -100
+ */
+
+function getMax(seconds) {
+    let timerMinutes = Math.floor(seconds / 60);
+    let timerSeconds = seconds % 60;
+
+    if (timerMinutes.toString().length === 1) {
+        timerMinutes = '0' + timerMinutes;
+    }
+    return timerMinutes + ':' + timerSeconds;
+}
+
+console.log(getMax([-10]));
+
 
 // console.log(getMax([-100, -200, 3]));
 
@@ -109,14 +201,14 @@
 
 // console.log(removeApples(['Banana', 'Apple', 'Orange', 'Apple']));
 
-function filterOutFalsy(arr) {
-    let truthyArr = [];
-    for (let i = 0; i < arr.length; ++i) {
-        console.log(arr[i])
-        if (!!arr[i] === true) {
-            truthyArr.push(arr[i]);
-        }
-    }
-}
+// function filterOutFalsy(arr) {
+//     let truthyArr = [];
+//     for (let i = 0; i < arr.length; ++i) {
+//         console.log(arr[i])
+//         if (!!arr[i] === true) {
+//             truthyArr.push(arr[i]);
+//         }
+//     }
+// }
 
-console.log(filterOutFalsy(['', NaN, 500, null, false, 0]));
+// console.log(filterOutFalsy(['', NaN, 500, null, false, 0]));
